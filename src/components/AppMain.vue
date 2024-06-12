0f161de41c3bae04a749f7b58ebf6e2a8519bcc8
valeriogonini <valeriogonini@gmail.com>
2024-06-12 17:19:09 +0200

    add app main enad methods

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        Eccoci
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProject() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then((res) => {
+                    console.log(res.data.results.data)
+
+                })
+        }
+
+    },
+    created() {
+        this.fetchProject()
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
